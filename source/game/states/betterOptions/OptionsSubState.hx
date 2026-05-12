f731212030427f7412a8e3cc44b12e0292b1623f
Makr <ngrom33@gmail.com>
2026-05-12 10:11:03 -0700

    я люблю булочку с маком

--- source/game/states/betterOptions/OptionsSubState.hx
+++ source/game/states/betterOptions/OptionsSubState.hx
@@ -367,6 +367,8 @@ class ArrowPress extends FlxSprite
 		updateHitbox();
 		animation.play('idle');
 		color = 0xFF333333;
+		#if MAKR_BUILD
+			color = 0xFFFF0000;
 		origScale = (scale.x + scale.y) / 2;
 		FlxMouseEvent.add(this, _ -> onPress(), _ -> onRelease(), /* null, */_ -> onOver(), _ -> onOut(), false, true, false);
 	}
